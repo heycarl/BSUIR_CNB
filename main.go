61c1c2d74cf65379412e48fdc74522a4ba91d5aa
Alexandr <efimchiksasha@gmail.com>
2023-09-10 02:19:51 +0300

    add UI for speed and port select implement port-scanning

--- main.go
+++ main.go
@@ -7,23 +7,71 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"go.bug.st/serial"
+	"log"
+	"strconv"
 )
+
+func scanPorts() []string {
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(ports) == 0 {
+		log.Fatal("No serial ports found!")
+	}
+	return ports
+}
+
+func serializeIntSlice(input []int) []string {
+	var valuesText []string
+	for i := range input {
+		number := input[i]
+		text := strconv.Itoa(number)
+		valuesText = append(valuesText, text)
+	}
+	return valuesText
+}
 
 func main() {
 	a := app.New()
-	ico, _ := fyne.LoadResourceFromPath(".\\resources\\ocn.ico")
+	ico, _ := fyne.LoadResourceFromPath(".\\resources\\icon.png")
 	a.SetIcon(ico)
 	a.Settings().SetTheme(colorTheme.GreyTheme())
 
 	window := a.NewWindow("OCN LAB1")
 	window.Resize(fyne.Size{Width: 400, Height: 250})
 
-	dropdown := widget.NewSelect(
-		[]string{"COM4", "COM5"},
+	rxDropdown := widget.NewSelect(
+		scanPorts(),
 		func(value string) {
-			fmt.Println("Selected:", value)
+			fmt.Println("RX port changed:", value)
 		},
 	)
+	txDropdown := widget.NewSelect(
+		scanPorts(),
+		func(value string) {
+			fmt.Println("TX port changed:", value)
+		},
+	)
+
+	availableSpeeds := []int{9600, 115200, 800}
+	rxSpeedDropdown := widget.NewSelect(
+		serializeIntSlice(availableSpeeds),
+		func(value string) {
+			fmt.Println("RX Speed changed:", value)
+		},
+	)
+	txSpeedDropdown := widget.NewSelect(
+		serializeIntSlice(availableSpeeds),
+		func(value string) {
+			fmt.Println("TX Speed changed:", value)
+		},
+	)
+	txDropdown.SetSelectedIndex(0)
+	rxDropdown.SetSelectedIndex(1)
+	txSpeedDropdown.SetSelectedIndex(0)
+	rxSpeedDropdown.SetSelectedIndex(0)
 
 	input := widget.NewEntry()
 	input.SetPlaceHolder("Type text to transfer")
@@ -34,15 +82,14 @@ func main() {
 		fmt.Println("Start button clicked")
 	})
 
-	buttonStop := widget.NewButton("Stop", func() {
-		fmt.Println("Stop button clicked")
-	})
-
 	l := container.NewVBox(
-		dropdown,
+		container.NewHBox(widget.NewLabel("RX port:"), rxDropdown),
+		container.NewHBox(widget.NewLabel("TX port:"), txDropdown),
+		container.NewHBox(widget.NewLabel("RX speed:"), rxSpeedDropdown),
+		container.NewHBox(widget.NewLabel("TX speed:"), txSpeedDropdown),
 		input,
 		text,
-		container.NewHBox(buttonStart, buttonStop),
+		buttonStart,
 	)
 
 	window.SetContent(l)
