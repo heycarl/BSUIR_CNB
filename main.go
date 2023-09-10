1d065b5f6cf3cc1e43228829b18b9f6757f6772f
Alexandr <efimchiksasha@gmail.com>
2023-09-11 00:17:33 +0300

    implement send and receive logic, link UI with controllers

--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
 	"go.bug.st/serial"
 	"log"
 	"strconv"
+	"strings"
 )
 
 func scanPorts() []string {
@@ -33,6 +35,52 @@ func serializeIntSlice(input []int) []string {
 	return valuesText
 }
 
+func sendStringToSerial(portName string, mode serial.Mode, msg string) {
+	port, err := serial.Open(portName, &mode)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	n, err := port.Write([]byte(msg))
+	log.Printf("Written %d bytes\n", n)
+	err = port.Close()
+	if err != nil {
+		return
+	}
+}
+
+func receiveStringFromSerial(portName string, mode serial.Mode, term rune) (string, error) {
+	port, rxErr := serial.Open(portName, &mode)
+	if rxErr != nil {
+		log.Printf("Port %s open failed\n", portName)
+		return "", rxErr
+	}
+
+	buff := make([]byte, 256)
+	var rxBytes = 0
+	var err error
+	for {
+		rxBytes, err = port.Read(buff)
+		if err != nil {
+			log.Fatal(err)
+			return "", err
+		}
+		if rxBytes == 0 {
+			log.Println("\nEOF")
+			break
+		}
+		if strings.Contains(string(buff[:rxBytes]), string(term)) {
+			break
+		}
+	}
+	err = port.Close()
+	if err != nil {
+		return "", err
+	}
+	log.Printf("Recieved %d bytes\n", rxBytes)
+	return string(buff), nil
+}
+
 func main() {
 	a := app.New()
 	ico, _ := fyne.LoadResourceFromPath(".\\resources\\icon.png")
@@ -42,30 +90,38 @@ func main() {
 	window := a.NewWindow("OCN LAB1")
 	window.Resize(fyne.Size{Width: 400, Height: 250})
 
+	var rxPortName string
+	var txPortName string
 	rxDropdown := widget.NewSelect(
 		scanPorts(),
-		func(value string) {
-			fmt.Println("RX port changed:", value)
+		func(v string) {
+			rxPortName = v
+			fmt.Println("RX port changed:", v)
 		},
 	)
 	txDropdown := widget.NewSelect(
 		scanPorts(),
-		func(value string) {
-			fmt.Println("TX port changed:", value)
+		func(v string) {
+			txPortName = v
+			fmt.Println("TX port changed:", v)
 		},
 	)
 
-	availableSpeeds := []int{9600, 115200, 800}
+	availableSpeeds := []int{9600, 115200, 50}
+	var rxSpeed = availableSpeeds[0]
+	var txSpeed = availableSpeeds[0]
 	rxSpeedDropdown := widget.NewSelect(
 		serializeIntSlice(availableSpeeds),
-		func(value string) {
-			fmt.Println("RX Speed changed:", value)
+		func(v string) {
+			rxSpeed, _ = strconv.Atoi(v)
+			log.Println("RX Speed changed:", v)
 		},
 	)
 	txSpeedDropdown := widget.NewSelect(
 		serializeIntSlice(availableSpeeds),
-		func(value string) {
-			fmt.Println("TX Speed changed:", value)
+		func(v string) {
+			txSpeed, _ = strconv.Atoi(v)
+			log.Println("TX Speed changed:", v)
 		},
 	)
 	txDropdown.SetSelectedIndex(0)
@@ -74,12 +130,29 @@ func main() {
 	rxSpeedDropdown.SetSelectedIndex(0)
 
 	input := widget.NewEntry()
+	var inputMessage string
+	inputMessageBinding := binding.BindString(&inputMessage)
+	input.Bind(inputMessageBinding)
 	input.SetPlaceHolder("Type text to transfer")
 
-	text := widget.NewLabel("Received text will be here")
+	receivedText := widget.NewLabel("Received text will be here")
+	var receivedMessage = "Received text will be here"
+	receivedMessageBinding := binding.BindString(&receivedMessage)
+	receivedText.Bind(receivedMessageBinding)
 
-	buttonStart := widget.NewButton("Start", func() {
-		fmt.Println("Start button clicked")
+	buttonStart := widget.NewButton("Send", func() {
+		msg, _ := inputMessageBinding.Get()
+		sendStringToSerial(txPortName, serial.Mode{BaudRate: txSpeed}, msg+"\n")
+		rxMsg, err := receiveStringFromSerial(rxPortName, serial.Mode{BaudRate: rxSpeed}, '\n')
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		err = receivedMessageBinding.Set(rxMsg)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
 	})
 
 	l := container.NewVBox(
@@ -88,70 +161,10 @@ func main() {
 		container.NewHBox(widget.NewLabel("RX speed:"), rxSpeedDropdown),
 		container.NewHBox(widget.NewLabel("TX speed:"), txSpeedDropdown),
 		input,
-		text,
+		receivedText,
 		buttonStart,
 	)
 
 	window.SetContent(l)
 	window.ShowAndRun()
 }
-
-//package main
-//
-//import (
-//	"go.bug.st/serial"
-//	"log"
-//	"strings"
-//)
-//
-//func main() {
-//	ports, err := serial.GetPortsList()
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	if len(ports) == 0 {
-//		log.Fatal("No serial ports found!")
-//	}
-//	for _, port := range ports {
-//		log.Printf("Found port: %v\n", port)
-//	}
-//
-//	rxMode := &serial.Mode{
-//		BaudRate: 115200,
-//	}
-//	rxPort, err := serial.Open("COM3", rxMode)
-//	txMode := &serial.Mode{
-//		BaudRate: 9600,
-//	}
-//	txPort, err := serial.Open("COM4", txMode)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	n, err := txPort.Write([]byte("Hello, world!\n"))
-//	if err != nil {
-//		log.Println(err)
-//		return
-//	}
-//	log.Printf("Wrote %d bytes\n", n)
-//
-//	log.Println("Received message: ")
-//	for {
-//		buff := make([]byte, 256)
-//		n, err := rxPort.Read(buff)
-//		if err != nil {
-//			log.Fatal(err)
-//		}
-//		if n == 0 {
-//			log.Println("\nEOF")
-//			break
-//		}
-//
-//		log.Printf("%s", string(buff[:n]))
-//
-//		if strings.Contains(string(buff[:n]), "\n") {
-//			break
-//		}
-//	}
-//
-//}
